66fe6f14a493cfc744c7a444f3ecf9067f21d3bb
Scott Ballantyne <scottballantyne@protonmail.com>
2025-09-11 11:48:22 -0500

    added gulp so I could make it even faster

---
+++ gulpfile.js
@@ -0,0 +1,39 @@
+const gulp = require('gulp');
+const { series, parallel } = require('gulp');
+
+
+var concat = require('gulp-concat');
+
+gulp.task('copy', function() {
+  return gulp.src([
+    './dependencies/handlebars.js',
+    './dependencies/jquery.js',
+    './dependencies/underscore.js',
+    './dependencies/backbone.js',
+    './dependencies/bootstrap.js',
+    './dependencies/openpgp.js'
+
+  ])
+    .pipe(gulp.dest('./public/'));
+
+})
+
+gulp.task('scripts', function() {
+  return gulp.src([
+    './dependencies/handlebars.js',
+    './dependencies/jquery.js',
+    './dependencies/underscore.js',
+    './dependencies/backbone.js',
+    './dependencies/bootstrap.js',
+    './dependencies/openpgp.js'
+
+  ])
+    .pipe(concat('dependencies.js'))
+    .pipe(gulp.dest('./public/'));
+});
+
+
+gulp.task('default', parallel("copy", "scripts"));
+
+
+

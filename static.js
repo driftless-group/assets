2465e0ade7023427635faa4f25538a7b01c6986f
Scott Ballantyne <scottballantyne@protonmail.com>
2025-09-18 11:58:59 -0500

    wrong path

--- static.js
+++ static.js
@@ -3,8 +3,8 @@ const fs   = require('fs');
 
 const defaultPath = path.join(process.cwd(), 'public')
 
-let gather = require(path.join(__dirname, '..', 'gather'));
-let finder = require(path.join(__dirname, '..', 'finder'));
+let gather = require(path.join(__dirname, 'gather'));
+let finder = require(path.join(__dirname, 'finder'));
 
 module.exports = function(options={}) {
   

12bcb7c24ca034e26241b78ad725ea95c6104a02
Scott Ballantyne <scottballantyne@protonmail.com>
2025-09-18 11:45:49 -0500

    I wonder if I can use a promise to make this asyncronous, does it work like that?

--- finder.js
+++ finder.js
@@ -1,14 +1,23 @@
 const path = require('fs');
 const fs   = require('fs');
 
-function finder(directories, file) {
-  return JSON.parse(JSON.stringify(directories)).reduce((found, directory) => {
+function exists(file) {
+  return new Promise((resolve) => {
     var filepath = path.join(directory, file);
-    if (fs.existsSync(filepath)) {
-      found.push(filepath)
-    }
+    resolve(fs.existsSync(file));
+  })
+} 
 
-    return found;
+function finder(directories, file) {
+  return JSON.parse(JSON.stringify(directories)).reduce(async(files, directory) => {
+    if (files.length == 0) {
+      var filepath = path.join(directory, file);
+      var found = await exists(filepath);
+      if (found) {
+        files.push(filepath)
+      }
+    }
+    return files;
   }, [])
 }
 

859fe363708ba935ec8a0ba6bb3898c26ee19646
Scott Ballantyne <scottballantyne@protonmail.com>
2025-09-18 13:10:08 -0500

    tidy

--- static.js
+++ static.js
@@ -3,7 +3,6 @@ const fs   = require('fs');
 const crypto = require('crypto');
 
 const defaultPath = path.join(process.cwd(), 'public')
-
 let finder = require(path.join(__dirname, 'finder'));
 
 module.exports = function(options={}) {
@@ -38,9 +37,8 @@ module.exports = function(options={}) {
     if (file == undefined) {
       return next();
     }
-    
-    fs.stat(file.path, function(err, stat) {
 
+    fs.stat(file.path, function(err, stat) {
       file.modified = stat.mtimeMs
       var etag = crypto.createHash('md5').update(JSON.stringify(file)).digest('hex');
       res.set('Etag', etag);
@@ -48,7 +46,6 @@ module.exports = function(options={}) {
       if (req.headers['if-none-match'] == etag) {
         res.status(304).end();
       } else {
-   
         if (options.cache) {
           res.set('Cache-Control', 'public, max-age=31536000'); // Cache for 1 year
         }

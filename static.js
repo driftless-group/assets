3e1dedc58cf8e71155bf8269acb42f24e99cf4a2
Scott Ballantyne <scottballantyne@protonmail.com>
2025-09-18 13:07:34 -0500

    removing unused function

--- static.js
+++ static.js
@@ -4,7 +4,6 @@ const crypto = require('crypto');
 
 const defaultPath = path.join(process.cwd(), 'public')
 
-let gather = require(path.join(__dirname, 'gather'));
 let finder = require(path.join(__dirname, 'finder'));
 
 module.exports = function(options={}) {
@@ -29,40 +28,36 @@ module.exports = function(options={}) {
       return next();
     }
 
-    var files = finder(options.path, req.url), file;
+    var file;
+    var files = finder(options.path, req.url);
     
     if (files.length > 0) {
       file = {host: req.host, url: req.url, path: files[0]}
     }
 
-    // maybe I should cache things in production?
-
-    if (file != undefined) {
-      fs.stat(file.path, function(err, stat) {
-        
-        file.modified = stat.mtimeMs
-        var etag = crypto.createHash('md5').update(JSON.stringify(file)).digest('hex');
-        res.set('Etag', etag);
-        
-        if (req.headers['if-none-match'] == etag) {
-          res.status(304).end();
-        } else {
-
-          if (options.cache) {
-            res.set('Cache-Control', 'public, max-age=31536000'); // Cache for 1 year
-          }
-
-          const readStream = fs.createReadStream(file.path);
-          readStream.pipe(res);
-        }
-      }); 
-
-    } else {
-      next();
+    if (file == undefined) {
+      return next();
     }
+    
+    fs.stat(file.path, function(err, stat) {
+
+      file.modified = stat.mtimeMs
+      var etag = crypto.createHash('md5').update(JSON.stringify(file)).digest('hex');
+      res.set('Etag', etag);
+
+      if (req.headers['if-none-match'] == etag) {
+        res.status(304).end();
+      } else {
+   
+        if (options.cache) {
+          res.set('Cache-Control', 'public, max-age=31536000'); // Cache for 1 year
+        }
+
+        const readStream = fs.createReadStream(file.path);
+        readStream.pipe(res);
+      }
+    }); 
+
   }
-
 }
-
-
 

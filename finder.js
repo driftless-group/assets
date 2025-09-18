f5cbce6d034f80ffc09de61c59ecb170947c79c1
Scott Ballantyne <scottballantyne@protonmail.com>
2025-09-18 11:54:24 -0500

    changing it back

--- finder.js
+++ finder.js
@@ -1,23 +1,14 @@
 const path = require('fs');
 const fs   = require('fs');
 
-function exists(file) {
-  return new Promise((resolve) => {
-    var filepath = path.join(directory, file);
-    resolve(fs.existsSync(file));
-  })
-} 
-
 function finder(directories, file) {
-  return JSON.parse(JSON.stringify(directories)).reduce(async(files, directory) => {
-    if (files.length == 0) {
-      var filepath = path.join(directory, file);
-      var found = await exists(filepath);
-      if (found) {
-        files.push(filepath)
-      }
+  return JSON.parse(JSON.stringify(directories)).reduce((found, directory) => {
+    var filepath = path.join(directory, file);
+    if (fs.existsSync(filepath)) {
+      found.push(filepath)
     }
-    return files;
+
+    return found;
   }, [])
 }
 

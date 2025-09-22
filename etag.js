bcbc06ea4fa683c4c9c0c4ecb6dae45174b40c09
Scott Ballantyne <scottballantyne@protonmail.com>
2025-09-22 06:45:49 -0500

    spaces

--- etag.js
+++ etag.js
@@ -4,8 +4,8 @@ function check(details={}, req, res, next) {
   if (typeof details != 'string') {
     details = JSON.stringify(details);
   }
+  
   var etag = crypto.createHash('md5').update(details).digest('hex');
-
   res.set('Etag', etag);
 
   if (req.headers['if-none-match'] == etag) {

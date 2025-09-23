378ab9ec4f3a097e84f36005389dd51444e3e9c4
Scott Ballantyne <scottballantyne@protonmail.com>
2025-09-23 09:01:03 -0500

    recompiling

--- public/dependencies.js
+++ public/dependencies.js
@@ -61825,10 +61825,83 @@ var openpgp = (function (exports) {
 })({});
 
 
-var helpers = {};
+if (typeof process === 'object') {
+  var path = require('path');
+  var Handlebars = require('handlebars');
+}
 
-helpers.t = function(name, options={}) {
-  console.log(name, options);
+if (helpers == undefined) { 
+  var helpers = {};
+}
+
+
+
+helpers.a = function() {
+  var args = Object.keys(arguments).map((index) => { 
+    return arguments[index]
+  }).filter((item) => {
+    return typeof item == 'string'
+  })
+  
+  var name = args.shift();
+
+  var link = "<a " + args.reduce((string, part) => {
+    var appendEquals = false;
+
+    if (string[string.length-1] != '=') { 
+      appendEquals = true;
+    }
+    
+    //part = Handlebars.escapeExpression(part)
+
+    if (appendEquals) {
+      string.push(part);
+      string.push('=');  
+    } else {
+      if (typeof part == 'string') {
+        string.push("'"+part+"'");
+      } else {
+        string.push(part);
+      }
+    }
+    
+    return string;
+  }, []).join('') + ">" + Handlebars.escapeExpression(name) + "</a>";
+  
+  return new Handlebars.SafeString(link);
+}
+
+helpers.script = function() {
+  var args = Object.keys(arguments).map((index) => { 
+    return arguments[index];
+  }).filter((item) => {
+    return typeof item == 'string';
+  })
+  
+  var scripts = args.reduce((stack, url) => {
+    stack.push('<script src="'+Handlebars.escapeExpression(url)+'"></script>');
+
+    return stack;
+  }, []).join('\n');
+  
+
+  return new Handlebars.SafeString(scripts)
+}
+
+helpers.style = function() {
+  var args = Object.keys(arguments).map((index) => { 
+    return arguments[index];
+  }).filter((item) => {
+    return typeof item == 'string';
+  })
+  
+  var sheets = args.reduce((stack, url) => {
+    stack.push('<link rel="stylesheet" href="'+Handlebars.escapeExpression(url)+'">');
+
+    return stack;
+  }, []).join('\n');
+
+  return new Handlebars.SafeString(sheets)
 }
 
 
@@ -61837,7 +61910,7 @@ helpers.t = function(name, options={}) {
 
 
 
-if (module != undefined) {
+if (typeof module != 'undefined') {
   module.exports = helpers;
 }
 

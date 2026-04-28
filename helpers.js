874f69a816f9bb305274a4e3ffa7768a1ecac0cb
Scott Ballantyne <scottballantyne@protonmail.com>
2026-04-28 14:13:56 -0500

    I understand why I might want all the translations in the view but I dont want them in the locals

--- helpers.js
+++ helpers.js
@@ -15,7 +15,7 @@ helpers.t = function(name, data={}, options={}) {
   //console.log(this.translations);
   //console.log(this.locale);
   
-  string = this.translations[name]
+  string = this.translations[this.locale];
 
   return string;
 }

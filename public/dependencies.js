c7b65197d71ba6b42fc02e194dc6b1cd48881e14
Scott Ballantyne <scottballantyne@protonmail.com>
2026-06-04 10:30:38 -0500

    making some styles be in all projects so that things become more consistent.

--- public/dependencies.js
+++ public/dependencies.js
@@ -65496,12 +65496,33 @@ function setMapLanguage(languageCode) {
 if (typeof process === 'object') {
   var path = require('path');
   var Handlebars = require('handlebars');
-  var marked = require('marked');
+  const { marked } = require('marked');
+  var pluralize = require('@drifted/pluralize');
 }
 
 if (helpers == undefined) { 
   var helpers = {};
+  
 }
+
+
+if (pluralize != undefined) {
+  helpers.pluralize = pluralize;
+} else {
+  var irregulars = {
+    person: 'people',
+    woman: 'women'
+  }
+
+  helpers.pluralize = function pluralize(noun, count) {
+    if (count == 1) {
+      return noun;
+    } else {
+      return (irregulars[noun] || noun + "s");
+    }
+  }
+}
+
 
 // this is just an initial stab at it.  
 // i think it might need to render handlebars etc.
@@ -65638,6 +65659,67 @@ helpers.hasRole = function(requiredRole, options) {
 
 helpers.hostname = function(name, options) {
   return new Handlebars.SafeString(options.data.root.urls.proto + "://" + options.data.root.urls[name]);
+}
+
+
+helpers.objectDir = function(obj, options) {
+  if (['ar', 'fa', 'ug'].indexOf(obj.locale) > -1) {
+    return "rtl";
+  } else {
+    return 'ltr';
+  }
+}
+
+
+helpers.setLocale = function(locale, options) {
+  var augmented = [options.data.root.currentUrl];
+  
+  if (options.data.root.currentUrl.indexOf('?') > -1) {
+    augmented.push("&");
+  } else {
+    augmented.push("?");
+  }
+  
+  augmented.push('setLocale='+locale);
+
+  return augmented.join('');
+}
+
+helpers.capitalize = function(str, options) {
+  if (!str) return str; // Handle empty strings
+  return str.charAt(0).toUpperCase() + str.slice(1);
+}
+
+helpers.header = function(name, options={}) {
+  var html = `<div class="d-flex justify-content-between align-items-center">
+    <h3>{{t plural title}}</h3>
+    {{#if new}}
+    <a href='{{prefix}}/new' class="btn btn-secondary">{{t new_section alt_button}}</a>
+    {{/if}}
+  </div>`
+
+  if (options.data.root.header == undefined) {
+    options.data.root.header = {};
+  }
+
+  var data = {
+    section: name,
+    new: true,
+    translations: options.data.root.translations,
+    plural: helpers.pluralize(2, name),
+    title: helpers.capitalize(helpers.pluralize(name, 2)),
+    new_section: "new_"+name,
+    alt_button: "New "+helpers.capitalize(name)
+  }
+
+  Object.assign(data, options.data.root.header);
+
+  if (data.prefix == undefined) {
+    data.prefix = ['/',data.plural].join('')
+  }
+
+  var template = Handlebars.compile(html)
+  return new Handlebars.SafeString(template(data));
 }
 
 
